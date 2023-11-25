dfbbc3f5b0f2eb1281bbe3244875a0b0f9866049
Kilian y Aridane <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-11-25 11:05:36 +0000

    feat: GetAll Cat pagination info

--- models/cat.model.js
+++ models/cat.model.js
@@ -4,10 +4,27 @@ export class CatModel {
     static async getAll(req) {
         try {
             const { pageSize, page } = req.query;
+
+            const countQuery = 'SELECT COUNT(*) as totalCount FROM cat';
+            const countResult = await db.query(countQuery);
+            const totalCount = countResult[0][0].totalCount;
+            const totalPages = Math.ceil(totalCount / pageSize);
+
             const offset = (page - 1) * pageSize;
-            const query = `SELECT * FROM cat LIMIT ${pageSize} OFFSET ${offset}`;
-            const cats = await db.query(query);
-            return cats[0];
+            const dataQuery = `SELECT * FROM cat LIMIT ${pageSize} OFFSET ${offset}`;
+            const cats = await db.query(dataQuery);
+
+            const result = {
+                data: cats[0],
+                pagination: {
+                    page,
+                    pageSize,
+                    totalCount,
+                    totalPages
+                }
+            };
+
+            return result;
         } catch (error) {
             console.error('Error fetching cats:', error);
             throw error;
