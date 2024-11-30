610f80482609a44753a8e3eada41439238ab0b48
Aridane Martín <aridanemartin@gmail.com>
2024-11-30 17:17:23 +0000

    feat: Implement image cache in imageService

--- services/imageService.js
+++ services/imageService.js
@@ -4,7 +4,11 @@ import {
     DeleteObjectCommand
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import NodeCache from 'node-cache';
 import r2 from './s3client/s3client.js';
+
+// Create a cache instance with a default TTL (time-to-live) of 1 hour
+const imageCache = new NodeCache({ stdTTL: 3600 });
 
 export const uploadImage = async (file, id) => {
     const buffer = file.buffer;
@@ -19,6 +23,8 @@ export const uploadImage = async (file, id) => {
     try {
         await r2.send(putObjectCommand);
         const imageUrl = `${process.env.CLOUDFARE_ENDPOINT}/${process.env.CLOUDFARE_BUCKET_NAME}/images/${id}`;
+
+        imageCache.set(id, imageUrl);
         return imageUrl;
     } catch (error) {
         throw error;
@@ -26,6 +32,11 @@ export const uploadImage = async (file, id) => {
 };
 
 export const getImage = async (id) => {
+    const cachedImageUrl = imageCache.get(id);
+    if (cachedImageUrl) {
+        return cachedImageUrl;
+    }
+
     const getObjectCommand = new GetObjectCommand({
         Bucket: process.env.CLOUDFARE_BUCKET_NAME,
         Key: `images/${id}`
@@ -35,6 +46,8 @@ export const getImage = async (id) => {
         const r2ImageSignedUrl = await getSignedUrl(r2, getObjectCommand, {
             expiresIn: 60 * 60
         });
+
+        imageCache.set(id, r2ImageSignedUrl);
         return r2ImageSignedUrl;
     } catch (error) {
         throw error;
@@ -49,6 +62,8 @@ export const deleteImage = async (id) => {
 
     try {
         const data = await r2.send(deleteObjectCommand);
+
+        imageCache.del(id);
         return data;
     } catch (error) {
         throw error;
